ec467e76361e100336511405a96a9923ea3392d9
Zeeshan1234 <mayub9799@gmail.com>
2023-12-07 12:36:47 +0500

    build.sh all

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Dockerfiles for setting up a libc version-specific environment.
 
 Libc:
-    - 2.23
-    - 2.27
-    - 2.31
-    - 2.35
+- 2.23
+- 2.27
+- 2.31
+- 2.35
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#1/bin/sh
+#!/bin/bash
 
 Help(){
 	echo "pwndocker - v0.0.1";
@@ -9,6 +9,8 @@ Help(){
 	echo "  - 2.27";
 	echo "  - 2.31";
 	echo "  - 2.35";
+	echo "To build all images, run: ./$(basename "$0") all";
+	echo "Default image names are 'pwndocker:libc-<version>'";
 }
 
 
@@ -16,6 +18,24 @@ if [ $# -eq "0" ];
 then
 	Help;
 	exit 1
+fi
+
+if [ "$1" = "all" ]; then
+	for d in src/*/; do
+		echo "[-] Building ${d%/} image...";
+		trimmed_version="${d#src/}"
+		trimmed_version="${trimmed_version%/}"
+		# echo "docker build $d -t pwndocker:libc-$trimmed_version";
+		docker build $d -t pwndocker:libc-$trimmed_version;
+
+		if [ $? -eq "0" ]; then
+			echo "[+] $d Build complete!";
+		else
+			echo "[-] Build failed!";
+			exit 1
+		fi
+	done
+	exit 0
 fi
 
 # echo docker build ./$1 -t $2;
